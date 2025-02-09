e83f278651b324be8d8325283b1654658a2f9d28
jean <jean.santos.jesus@gmail.com>
2025-02-09 16:23:56 -0300

    0902
    Menu

---
+++ Assets/Game Folder/Scene/Trigger/Script/GameOver.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+
+    void OnEnable()
+    {
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<CanvasGroup>().alpha += Time.deltaTime * 0.5f;
+    }
+}
