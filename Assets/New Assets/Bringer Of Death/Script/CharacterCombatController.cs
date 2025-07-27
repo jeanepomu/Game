c2dafd24f26b6fb9b5678719444e52a1fd985f79
jean <jean.santos.jesus@gmail.com>
2025-07-27 13:58:25 -0300

    Bringer Enemy

--- Assets/New Assets/Bringer Of Death/Script/CharacterCombatController.cs
+++ Assets/New Assets/Bringer Of Death/Script/CharacterCombatController.cs
@@ -1,73 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterCombatController : MonoBehaviour
 {
-    public float damageAmount = 10f;
-    public float attackCooldown = 1.5f;
-    public float moveSpeed = 0.1f;
+    public Transform a;
+    public Transform b;
 
-    public Transform pointA;
-    public Transform pointB;
-    private Transform currentTarget;
+    //som do inimigo Keepeer
 
-    private float lastAttackTime;
-    private Animator animator;
+    public AudioSource audioSource;
+    public AudioClip dieSound;
 
+    public Transform skin;
+    public Transform bringerRange;
+
+    public bool goRight;
+
+    // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
-        currentTarget = pointB;
+
     }
 
+    // Update is called once per frame
     void Update()
     {
-        Patrol();
-
-        // Você pode adicionar outras condições aqui, tipo quando atacar etc.
-    }
-
-    void Patrol()
-    {
-        if (currentTarget == null) return;
-
-        Vector3 direction = currentTarget.position - transform.position;
-        transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, moveSpeed * Time.deltaTime);
-        transform.LookAt(currentTarget);
-
-        if (direction.magnitude > 0.1f)
+        if (GetComponent<Character>().life <= 0)
         {
-            animator.SetTrigger("Walk");
+
+            audioSource.PlayOneShot(dieSound, 0.5f);
+            bringerRange.GetComponent<CircleCollider2D>().enabled = false;
+            GetComponent<CapsuleCollider2D>().enabled = false;
+            this.enabled = false;
+        }
+
+        if (skin.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            return;
+        }
+
+
+        if (goRight == true)
+        {
+            skin.localScale = new Vector3(1, 1, 1);
+
+            if (Vector2.Distance(transform.position, b.position) < 0.1f)
+            {
+                goRight = false;
+
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, b.position, 1.5f * Time.deltaTime);
         }
         else
         {
-            // Alterna o destino ao chegar
-            currentTarget = (currentTarget == pointA) ? pointB : pointA;
-            animator.SetTrigger("Idle");
-        }
-    }
+            skin.localScale = new Vector3(-1, 1, 1);
 
-    public void Attack()
-    {
-        if (Time.time >= lastAttackTime + attackCooldown)
-        {
-            animator.SetTrigger("Attack");
-            lastAttackTime = Time.time;
-
-            Collider[] hitPlayers = Physics.OverlapSphere(transform.position + transform.forward, 0.5f);
-            foreach (var hit in hitPlayers)
+            if (Vector2.Distance(transform.position, a.position) < 0.1f)
             {
-                if (hit.CompareTag("Player")) 
-                {
-                    
-                }
-                   // hit.GetComponent<PlayerHealth>()?.TakeDamage(damageAmount);
-            }
-        }
-    }
+                goRight = true;
 
-    // Outras animações
-    public void Cast() => animator.SetTrigger("Cast");
-    public void Spell() => animator.SetTrigger("Spell");
-    public void Death() => animator.SetTrigger("Death");
-    public void Hurt() => animator.SetTrigger("Hurt");
+
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, a.position, 1.5f * Time.deltaTime);
+        }
+
+    }
 }
