33df1c534a86b659852024589e2e667a0b607e78
jean <jean.santos.jesus@gmail.com>
2025-08-30 23:48:27 -0300

    3008

---
+++ Assets/New Assets/Bringer Of Death/Script/BringerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BringerController : MonoBehaviour
+{
+    public Transform a;
+    public Transform b;
+
+    //som do inimigo Keepeer
+
+    public AudioSource audioSource;
+    public AudioClip dieSound;
+
+    public Transform skin;
+    public Transform bringerRange;
+
+    public bool goRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<Character>().life <= 0)
+        {
+
+            audioSource.PlayOneShot(dieSound, 0.5f);
+            bringerRange.GetComponent<CircleCollider2D>().enabled = false;
+            GetComponent<CapsuleCollider2D>().enabled = false;
+            this.enabled = false;
+        }
+
+        if (skin.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            return;        
+        }
+
+
+        if(goRight == true)
+        {
+            skin.localScale = new Vector3(-1, 1, 1);
+
+            if (Vector2.Distance(transform.position, b.position) < 0.1f)
+            {
+                goRight = false;
+                
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, b.position, 1.5f * Time.deltaTime);
+        }
+        else
+        {
+            skin.localScale = new Vector3(1, 1, 1);
+
+            if (Vector2.Distance(transform.position, a.position) < 0.1f)
+            {
+                goRight = true;
+
+
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, a.position, 1.5f * Time.deltaTime);
+        }
+        
+    }
+}
